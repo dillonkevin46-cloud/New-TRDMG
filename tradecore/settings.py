fce713f9d7e296a4fe783cb5fbd0d6e081ed9d5e
dillonkevin46-cloud <dillonkevin46@gmail.com>
2026-03-23 08:45:10 +0200

    updated

--- tradecore/settings.py
+++ tradecore/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = "tradecore.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME", "TRDMG_DB"),
+        "NAME": os.environ.get("DB_NAME", "tradecore_db"),
         "USER": os.environ.get("DB_USER", "postgres"),
-        "PASSWORD": os.environ.get("DB_PASSWORD", "Mortal@75"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
         "HOST": os.environ.get("DB_HOST", "localhost"),
         "PORT": os.environ.get("DB_PORT", "5432"),
     }
